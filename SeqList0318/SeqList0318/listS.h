369ae4ce9ffc4901879fd1ddf9569561a1c9a647
unknown <rhkdtjdtnrud@gmail.com>
2026-03-29 16:53:00 +0900

    first commit

---
+++ SeqList0318/SeqList0318/listS.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+//#ifndef _LISTS_H_
+//#define _LISTS_H_
+//#endif
+
+#define MAX 10
+
+int insertElement(int L[], int n, int x);
+
+int insertElementTwo(int L[], int n, int k, int x);
+
+int deleteElement(int L[], int n, int x);
+
+
---
+++ SeqList0318/SeqList0318/seqMain.c
@@ -0,0 +1,60 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <windows.h>
+#include "listS.h"
+
+int main(void)
+{
+    SetConsoleOutputCP(65001);//UTF-8,CP는 Code Page의 약자
+    SetConsoleCP(65001); // 입력도 필요하면 설정
+
+    int list[MAX] = { 10, 20, 40, 50, 60, 70 };
+    int i = 0, move = 0, size = 6; //size는 리스트에 있는 원소의 개수
+
+    printf("Start선형 리스트 원소 삽입과 삭제 연습\n");
+    for (i = 0; i < MAX; i++)
+        printf("list[%d]=%d\n", i, list[i]);
+
+    // 삽입 전
+    printf("\n삽입 전 선형 리스트 : ");
+    for (i = 0; i < size; i++)
+        printf("%3d ", list[i]);
+    printf("\n원소의 갯수 : %d \n", size);
+
+    // insertElement: 정렬 순서 기준 자동 삽입 (30 삽입)
+    move = insertElement(list, size, 30);
+
+    printf("\n삽입 후 선형 리스트 : ");
+    for (i = 0; i <= size; i++)
+        printf("%3d ", list[i]);
+    printf("\n원소의 갯수 : %d ", ++size);
+    printf("\n자리 이동 횟수 : %d \n", move);
+
+    // deleteElement: 30 삭제
+    printf("원소 30을 삭제전 size=%d\n", size);
+    move = deleteElement(list, size, 30);
+    if (move == size)
+        printf("\n선형 리스트에 원소가 없어서 삭제할 수 없습니다.\n");
+    else {
+        printf("\n삭제 후 선형 리스트 : ");
+        for (i = 0; i < size - 1; i++)
+            printf("%3d ", list[i]);
+        printf("\n원소의 갯수 : %d ", --size);
+        printf("\n자리 이동 횟수 : %d \n", move);
+    }
+
+    // insertElementTwo: 인덱스 직접 지정 삽입
+    int idx = 0, val = 0;
+    printf("\n원하는 배열의 인덱스와 원소값을 입력하시오 : ");
+    scanf("%d %d", &idx, &val);
+
+    move = insertElementTwo(list, size, idx, val);
+
+    printf("\n삽입 후 선형 리스트 : ");
+    for (i = 0; i <= size; i++)
+        printf("%3d ", list[i]);
+    printf("\n원소의 갯수 : %d ", ++size);
+    printf("\n자리 이동 횟수 : %d \n", move);
+
+    return 0;
+}
